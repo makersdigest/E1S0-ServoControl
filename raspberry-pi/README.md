3080494652b5be39605a42cae050dd1b12531454
Kaleb Clark <makersdigest@gmail.com>
2018-12-03 03:14:13 +0000

    Update README.md

--- raspberry-pi/README.md
+++ raspberry-pi/README.md
@@ -13,3 +13,22 @@ The wiringpi-complete.py example requires that software be installed on your pi.
 
 ### scipy module
 `sudo apt-get install python-scipy`
+
+## Find Servo Range
+We need to determine what the actual range of the servo is. The specs for the SG90 servo that we used in the video show that the pulse width range is 1ms to 2ms. That would translate in wiringpi to 100 to 200. When I tested that, the range was only 90 degrees of the 180 degree servo. Follow this guide to figure out what the actual pulse width range of your servo is.
+
+Note: You must install wiringpi first
+
+### Setup wiring pi for PWM
+`gpio -g mode 18 pwm`
+
+`gpio pwm-ms`
+
+`gpio pwmc 192`
+
+`gpio pwmr 2000`
+
+
+### Find lower range
+`gpio -g pwm 18 50`
+
--- raspberry-pi/wiringpi-example.py
+++ raspberry-pi/wiringpi-example.py
@@ -1,28 +1,41 @@
 ##
  # Maker's Digest
  # Servo Control
+ #
+ # Dont forget to install wiringpi and scipy! See README.md for details
 ##
-from time import sleep
-import wiringpi
-from scipy.interpolate import interp1d
+import wiringpi         # Import Wiringpi module
+from time import sleep  # Import sleep from time module
+from scipy.interpolate import interp1d  # scipy for interpolation
 
-pin = 18
+pin = 18                # Using pin 18 on Raspberry Pi
 
-pw_range = [50, 260]
+# Here we are going to setup our interpolation ranges.
+# To determine the pw_range, follow the directions in the README.md
+# deg_range, is the range of your servo. Leave as is if it is a 180 degree servo
+pw_range = [50, 260] 
 deg_range = [0, 180]
 interp_degrees = interp1d(deg_range, pw_range)
 
+# Setting up wiringpi. We are setting the pinmode to PWM Output, 
+# PWM Mode to MS (milliseconds), Clock and range set as they are 
+# tell wiringpi to run at 50hz. 
 wiringpi.wiringPiSetupGpio()
 wiringpi.pinMode(pin, wiringpi.GPIO.PWM_OUTPUT)
 wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
 wiringpi.pwmSetClock(192)
 wiringpi.pwmSetRange(2000)
 
+# This method takes input of degrees, converts it to pulse width,
+# waits half a second, then sets the pulse width to 0. In wiringpi
+# setting the pulsewidth to zero essentially turns off the servo. 
+# otherwise it will be jittery. 
 def setServoDegrees(deg):
     wiringpi.pwmWrite(pin, int(interp_degrees(deg)))
     sleep(0.5)
     wiringpi.pwmWrite(pin, 0)
     
+# run the example. 0 -> 90 -> 180 -> 90 -> 0 ...
 try:
     while True:
         setServoDegrees(0)
@@ -30,5 +43,4 @@ try:
         setServoDegrees(180)
         setServoDegrees(90)
 except KeyboardInterrupt:
-    quit()
-
+    wiringpi.pwmWrite(pin,0)
